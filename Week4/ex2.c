232f7b14dd4410007aeee47b5aeb7f55bf9d631a
QuangTran2706 <48042533+QuangTran2706@users.noreply.github.com>
2019-03-15 23:10:28 +0700

    Add files via upload

--- Week4/ex2.c
+++ Week4/ex2.c
@@ -1,27 +1,46 @@
 #include <stdio.h>
+#include <math.h>
 
-int main(int argc, char const *argv[]) {
+void main() {
     unsigned long n;
     printf("enter the amount of number: ");
     scanf("%ld", &n);
     unsigned long array[n];
+        //scan the number of the string
     for (int i = 0; i < n; i++) {
         printf("your number = ");
         scanf("%ld", &array[i]);
     }
-    //print in reverse order
-//     for (int i = n - 1; i >= 0; i--){
-//         printf("%ld\n", array[i]);
-//      }
-    //print follow order
-//      for (int i = 0; i < n; i++) {
-//          printf("%ld\n", array[i]);
-//      }
-    for (int i = 0; i < n; i++) {
-        int a = array[i] % 2;
-        if (a == 1) {
-            printf("%ld \n",array[i]);
-        }
-    }
-    return 0;
+        //print in reverse order
+    // for (int i = n - 1; i >= 0; i--){
+    //     printf("%ld\n", array[i]);
+    //  }
+        //print follow order
+    // for (int i = 0; i < n; i++) {
+    //      printf("%ld\n", array[i]);
+    //  }
+        //print odd number only
+    // for (int i = 0; i < n; i++) {
+    //     int a = array[i] % 2;
+    //     if (a == 1) {
+    //         printf("%ld \n",array[i]);
+    //     }
+    // }
+        //print the input bigger than 5
+
+        //find mean
+    // float sum = 0, mean;
+    // for (int i = 0; i < n; i++) {
+    //     sum += array[i];
+    // }
+    // mean = sum / n;
+    // printf("%f\n", mean);
+        //find deviation
+    // float deviation, sum2 = 0;
+    // for (int i = 0; i < n; i++) {
+    //     int a = (array[i] - mean) * (array[i] - mean) ;
+    //     sum2 = a + sum2;
+    // }
+    // deviation = sqrt(sum2 / (n - 1));
+    // printf("%f\n", deviation);
 }
