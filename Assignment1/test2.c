5be21afa2ca0c2c547fd27697bb613917033320a
Quang <s3757281@maclabsst-base.rmit.edu.vn>
2019-03-20 17:19:35 +0700

    dm

--- Assignment1/test2.c
+++ Assignment1/test2.c
@@ -3,9 +3,10 @@
 
 int main(int argc, char const *argv[]) {
     char phone_number[11];
-    printf("Enter your phone number");
+    printf("Enter your phone number:");
     scanf("%s", phone_number);
     if (phone_number[0] == '-') {
+        printf("program exit\n");
         //break;
         return 0;
     }
@@ -19,22 +20,22 @@ int main(int argc, char const *argv[]) {
     } else if (string_length == 11 && phone_number[1] != '2') {
             printf("Invalid phone number. Invalid area code\n");
     } else if (string_length == 10) {
-        char valid_mobile_code[50][2] = {"32", "33", "34", "35", "36", "37",
-                                    "38", "39", "52", "56", "58", "59",
-                                    "70", "76", "77", "78", "79", "81",
-                                    "82", "83", "84", "85", "86", "87",
-                                    "88", "89", "90", "91", "92", "93",
-                                    "94", "95", "96", "97", "98", "99" };
-        char a[2], b[2];
-        a[0] = phone_number[1];
-        b[0] = phone_number[2];
-        strcat(a, b);
-        for (int i = 0; i < sizeof(valid_mobile_code) / sizeof(valid_mobile_code[0]); i++) {
-            unsigned short j = strcmp(a, valid_mobile_code[i]);
-            if (j != 0) {
-                printf("Invalid phone number. Invalid mobile code\n");
+        int in = 0;
+        char valid_mobile_code[36][2] = {"32", "33", "34", "35", "36", "37",
+                                        "38", "39", "52", "56", "58", "59",
+                                        "70", "76", "77", "78", "79", "81",
+                                        "82", "83", "84", "85", "86", "87",
+                                        "88", "89", "90", "91", "92", "93",
+                                        "94", "95", "96", "97", "98", "99" };
+        for (int i = 0; i < (sizeof(valid_mobile_code) / sizeof(valid_mobile_code[0])); i++) {
+            if (phone_number[1] == valid_mobile_code[i][0] &&
+                phone_number[2] == valid_mobile_code[i][1]) {
+                in = 1;
                 break;
             }
+        }
+        if (in == 0) {
+            printf("Invalid phone number. Invalid mobile code\n");
         }
     } else printf("Your phone number is correct\n");
     return 0;
--- Assignment1/test3.c
+++ Assignment1/test3.c
@@ -2,27 +2,6 @@
 #include <string.h>
 
 int main(int argc, char const *argv[]) {
-    // char phone_number[11];
-    // printf("Enter your phone number");
-    // scanf("%s", phone_number);
-    //     char valid_mobile_code[] = {32, 33, 34, 35, 36, 37,
-    //                                 38, 39, 52, 56, 58, 59,
-    //                                 70, 76, 77, 78, 79, 81,
-    //                                 82, 83, 84, 85, 86, 87,
-    //                                 88, 89, 90, 91, 92, 93,
-    //                                 94, 95, 96, 97, 98, 99 };
-        // char mobile_code = strcat('3', '4');
-        char a, b;
-        a = 4;
-        b = 3;
-        strcat(a, b);
-        printf("%s\n", a);
-        // for (int i = 0, i < sizeof(valid_mobile_code) / sizeof(valid_mobile_code[0]); i++) {
-        //     unsigned short j = strcmp (mobile_code, valid_mobile_code);
-        //     if (j != 0) {
-        //         printf("Invalid phone number. Invalid mobile code\n");
-        //         break;
-        //     }
-        // }
+
     return 0;
 }
