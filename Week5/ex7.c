7f69070eb3204d9bf16979d0103b70e871f94ed8
Quang <s3757281@maclabsst-base.rmit.edu.vn>
2019-03-21 17:52:30 +0700

    done

--- Week5/ex7.c
+++ Week5/ex7.c
@@ -3,12 +3,15 @@
 
 int main(int argc, char const *argv[]) {
     char string[100];
+    char *q = string;
+
     printf("Enter a string: ");
     scanf("%[^\n]s", string);
-    char *q = string;
+
     for (int i = strlen(string); i >= 0; i--) {
         printf("%c",*(string + i));
     }
     printf("\n");
+
     return 0;
 }
