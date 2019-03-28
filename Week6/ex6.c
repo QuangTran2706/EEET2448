dd474662cfc03111269dae2f9c170dd95ddf48c1
Quang <s3757281@maclabsst-base.rmit.edu.vn>
2019-03-28 17:49:18 +0700

    dm

---
+++ Week6/ex6.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+
+int decimal_to_binary (int i)
+
+int main(int argc, char const *argv[]) {
+    int number[100];
+    printf("Enter your positive number: ");
+    scanf("%d", number);
+    decimal_to_binary
+    return 0;
+}
