aedcc5de4cd609eccaea50d72dfc1292cc716d1b
QuangTran2706 <s3757281@gmail.com>
2019-04-11 11:11:17 +0700

    format

--- Week6/ex5.c
+++ Week6/ex5.c
@@ -2,6 +2,7 @@
 
 int digit_sum (int n) {
     int sum = 0;
+
     while (n != 0) {
         sum += n % 10;
         n = n / 10;
--- Week6/ex6.c
+++ Week6/ex6.c
@@ -3,11 +3,13 @@
 void decimal_to_binary (int number) {
     int binumber[20];
     int i = 0;
+
     while (number > 0) {
         binumber[i] = number % 2;
         number = number / 2;
         i++;
     }
+
     for (int j = i - 1; j >= 0; j--) {
         printf("%d", binumber[j]);
     }
@@ -15,6 +17,7 @@ void decimal_to_binary (int number) {
 
 int main(int argc, char const *argv[]) {
     int number;
+
     printf("Enter your positive number: ");
     scanf("%d", &number);
     decimal_to_binary(number);
