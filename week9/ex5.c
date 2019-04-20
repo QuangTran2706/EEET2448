52375d0b9b4d4ba8f1b66d8f09452374b7d51734
QuangTran2706 <48042533+QuangTran2706@users.noreply.github.com>
2019-04-21 00:16:18 +0700

    ex5 week 9

--- week9/ex5.c
+++ week9/ex5.c
@@ -1,56 +1,40 @@
 #include <stdio.h>
 
-int main()
-{
- FILE *fp;
- char filename[100];
- char ch;
- int linecount, wordcount, charcount;
+int main(int argc, char const *argv[]) {
+    FILE *fp;
+    char ch;
+    int linecount, wordcount, charcount;
 
- // Initialize counter variables
- linecount = 0;
- wordcount = 0;
- charcount = 0;
+    linecount = 0;
+    wordcount = 0;
+    charcount = 0;
 
- // Prompt user to enter filename
-  printf("Enter a filename :");
-  gets(filename);
+    fp = fopen(argv[1],"r");
 
-    // Open file in read-only mode
-   fp = fopen(filename,"r");
-
-   // If file opened successfully, then write the string to file
-   if ( fp )
-   {
-	//Repeat until End Of File character is reached.
-	   while ((ch=getc(fp)) != EOF) {
-	   	  // Increment character count if NOT new line or space
-		    if (ch != ' ' && ch != '\n') { ++charcount; }
-
-		  // Increment word count if new line or space character
-		   if (ch == ' ' || ch == '\n') { ++wordcount; }
-
-		  // Increment line count if new line character
-		   if (ch == '\n') { ++linecount; }
-
-
-
-	   }
-
-	   if (charcount > 0) {
-		++linecount;
-		++wordcount;
-	   }
-    }
-   else
-      {
-         printf("Failed to open the file\n");
+    if ( fp ) {
+        while ((ch=getc(fp)) != EOF) {
+            if (ch != ' ' && ch != '\n') {
+                charcount++;
+            }
+            if (ch == ' ' || ch == '\n') {
+                wordcount++;
+            }
+            if (ch == '\n') { 
+                linecount++;
+            }
         }
+
+        if (charcount > 0) {
+            linecount++;
+            wordcount++;
+        }
+    } else {
+        printf("cannot open the file\n");
+    }
 
     printf("Lines : %d \n", linecount);
     printf("Words : %d \n", wordcount);
     printf("Characters : %d \n", charcount);
 
-getchar();
 return(0);
 }
